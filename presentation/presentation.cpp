203b72585e973b8c580518110473bcda3d95df92
Timmy <timmyliujian@gmail.com>
2017-06-11 20:55:45 -0700

    next is to choose the right seetings for the swap chain

--- presentation/presentation.cpp
+++ presentation/presentation.cpp
@@ -33,6 +33,12 @@ struct QueueFamilyIndices
 	}
 };
 
+struct SwapChainSupportDetails {
+	VkSurfaceCapabilitiesKHR capabilities;
+	std::vector<VkSurfaceFormatKHR> formats;
+	std::vector<VkPresentModeKHR> presentModes;
+};
+
 const bool enableValidationLayers = true;
 
 VkResult CreateDebugReportCallbackEXT(
@@ -69,6 +75,32 @@ private:
 		createSurface();
 		pickPhysicalDevice();
 		createLogicalDevice();
+	}
+
+	SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device)
+	{
+		SwapChainSupportDetails details;
+		//capabilities
+		vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, &(details.capabilities));
+		//formats
+		uint32_t formatCount;
+		vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, nullptr);
+		if (formatCount != 0)
+		{
+			details.formats.resize(formatCount);
+			vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, details.formats.data());
+		}
+		//present mode
+		uint32_t presentModeCount;
+		vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, nullptr);
+		if (presentModeCount != 0)
+		{
+			details.presentModes.resize(presentModeCount);
+			vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, details.presentModes.data());
+		}
+
+
+		return details;
 	}
 
 	bool checkDeviceExtensionSupport(VkPhysicalDevice device)
@@ -201,6 +233,16 @@ private:
 		bool extensionSupported = checkDeviceExtensionSupport(device);
 
 		bool_to_return = bool_to_return && (indices.isComplete()) && extensionSupported;
+
+		//check is swapChain good enough
+		bool swapChainAdequate = false;
+		if (extensionSupported)
+		{
+			SwapChainSupportDetails swapChainSupport = querySwapChainSupport(device);
+			swapChainAdequate = (!swapChainSupport.formats.empty()) &&
+				(!swapChainSupport.presentModes.empty());
+		}
+		bool_to_return = bool_to_return && swapChainAdequate;
 		return  bool_to_return;
 	}
 	void pickPhysicalDevice()
